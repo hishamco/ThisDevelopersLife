69da6f0ef5a13dd96ee66e1fd13f651982e2fe53
hishamco <hishamco_2007@yahoo.com>
2016-08-23 08:37:55 +0300

    Fix media urls

--- Services/InMemoryShowService.cs
+++ Services/InMemoryShowService.cs
@@ -48,13 +48,13 @@ namespace ThisDevelopersLife.Services
                     Slug ="4-0-1-Faith", 
                     Summary = "How does our Faith drive us? Do you have Faith in yourself and your abilities, and how do you know you're on the right path? ", 
                     LeadImage = "401-Faith.jpg", 
-                    MediaFile = "http://media", 
+                    MediaFile = "http://podtrac.com/pts/redirect.mp3/traffic.libsyn.com/devlife/401-Faith.mp3", 
                     PublishedAt = DateTime.Now
                 },
                 new Episode() {
                     Title = "3.0.2 Space", 
                     Slug ="3-0-2-Space", 
-                    Summary = "In this episode of This Developer's Life we ask the hard questions about space. Why aren't we on Mars? Why haven't we gone back to the moon? We talk to Paul Lutus, Dan Bricklin, Tamar Cohen, and hear from Ray Bradbury.", LeadImage = "302-Space.jpg", MediaFile = "http://media", 
+                    Summary = "In this episode of This Developer's Life we ask the hard questions about space. Why aren't we on Mars? Why haven't we gone back to the moon? We talk to Paul Lutus, Dan Bricklin, Tamar Cohen, and hear from Ray Bradbury.", LeadImage = "302-Space.jpg", MediaFile = "http://podtrac.com/pts/redirect.mp3/traffic.libsyn.com/devlife/302-Space.mp3", 
                     PublishedAt = DateTime.Now
                 },
                 new Episode() {
@@ -62,7 +62,7 @@ namespace ThisDevelopersLife.Services
                     Slug ="301-Cancer", 
                     Summary = "Mo and Scott share their thoughts and struggle in this cancer diary they started the day after Mo was diagnosed.", 
                     LeadImage = "301-Cancer.jpg", 
-                    MediaFile = "http://media", 
+                    MediaFile = "http://podtrac.com/pts/redirect.mp3/traffic.libsyn.com/devlife/301-Cancer.mp3", 
                     PublishedAt = DateTime.Now
                 },
                 new Episode() {
@@ -70,7 +70,7 @@ namespace ThisDevelopersLife.Services
                     Slug ="114-Obsession", 
                     Summary = "Rob Sullivan talks about his obsessiveness and how it helps him, and hinders him in his job. Rory Blythe and Scott then talk openly about Rory's compulsion to keep learning - which he has embraced completely.", 
                     LeadImage = "114-Obsession.jpg", 
-                    MediaFile = "http://media", 
+                    MediaFile = "http://podtrac.com/pts/redirect.mp3/traffic.libsyn.com/devlife/114_Obsession.mp3", 
                     PublishedAt = DateTime.Now
                }
             };
